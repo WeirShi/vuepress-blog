0ca8c3566d33585ab3e4e403fcb823d700e3242a
weirshi <woshiweir@yahoo.com>
2021-04-29 11:24:15 +0800

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -20,7 +20,7 @@ handler.on('push', function (event) {
   console.log('Received a push event for %s to %s',
     event.payload.repository.name,
     event.payload.ref)
-  rumCommand('sh', ['./deployed.sh'], function (txt) {
+  rumCommand('sh', ['./deploy.sh'], function (txt) {
     console.log(txt)
   })
 })
